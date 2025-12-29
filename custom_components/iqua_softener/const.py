6c1788b4307948d6207f45adb54ce61524505650
Sascha <137452657+bggsolar@users.noreply.github.com>
2025-12-29 22:14:53 +0100

    const.py aktualisieren

--- custom_components/iqua_softener/const.py
+++ custom_components/iqua_softener/const.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 DOMAIN = "iqua_softener"
 
-CONF_USERNAME = "username"   # email
+CONF_EMAIL = "email"
 CONF_PASSWORD = "password"
 CONF_DEVICE_UUID = "device_uuid"
 
+# Unit strings (Home Assistant shows these as text units)
 VOLUME_FLOW_RATE_LITERS_PER_MINUTE = "L/min"
-VOLUME_FLOW_RATE_GALLONS_PER_MINUTE = "gal/min"
