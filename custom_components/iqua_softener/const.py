087f185e14124ceca22b81dc620e1586adf47aae
Sascha <137452657+bggsolar@users.noreply.github.com>
2025-12-29 17:49:40 +0100

    Rename device serial number to device UUID

--- custom_components/iqua_softener/const.py
+++ custom_components/iqua_softener/const.py
@@ -4,7 +4,7 @@ DOMAIN: Final = "iqua_softener"
 
 CONF_USERNAME: Final = "username"
 CONF_PASSWORD: Final = "password"
-CONF_DEVICE_SERIAL_NUMBER: Final = "device_sn"
+CONF_DEVICE_UUID: Final = "device_uuid"
 
 VOLUME_FLOW_RATE_LITERS_PER_MINUTE: Final = "L/m"
 VOLUME_FLOW_RATE_GALLONS_PER_MINUTE: Final = "gal/m"
